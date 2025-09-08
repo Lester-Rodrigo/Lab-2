5802c6bd90e289bc8dbdc02475254384bd7f6d2c
Lester-Rodrigo <alrodrinew@gmail.com>
2025-09-08 11:06:49 -0600

    Comienzo del main y de el tablero

--- Main.java
+++ Main.java
@@ -1,5 +1,46 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+    //Atributos para permitir recorrer el programa
+    Scanner input = new Scanner(System.in);
+    int pagina;
+    // Atributos para el jugador
+    String nombreJ1;
+    String nombreJ2;
+    // Atributos para crear el tablero
+    int tamano;
+
+        do {
+            do{
+                System.out.println("1. Iniciar juego: ");
+                System.out.println("2. Agregar simbolos: ");
+                System.out.println("3. Salir: ");
+                System.out.println("Ingrese alguna opción (Del 1 al 3): ");
+                pagina = input.nextInt();
+            }while (pagina < 1 && pagina > 3 );
+            switch (pagina) {
+
+                case 1:
+                    Scanner datos = new Scanner(System.in);
+                    System.out.println("Ingrese el nombre del jugador 1: ");
+                    nombreJ1 = datos.nextLine();
+                    System.out.println("Ingrese el nombre del jugador 2: ");
+                    nombreJ2 = datos.nextLine();
+                    System.out.println("Ingrese el tamaño del tablero: ");
+                    tamano = datos.nextInt();
+                    break;
+            
+                case 2:
+                    datos = new Scanner(System.in);
+                    System.out.println("Ingrese el nuevo simbolo: ");
+                    nombreJ1 = datos.nextLine();
+                    break;
+
+                case 3:
+                    System.exit(0);
+                    break;    
+            }
+        } while (pagina != 2 );
     }
 }
--- Tablero.java
+++ Tablero.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+
 public class Tablero {
-    
+    private int tamano;
+    private ArrayList<Carta> Baraja = new ArrayList<>();
+    private ArrayList<String> Simbolos = new ArrayList<>();
+    private final Carta [] [] cartas;
+
+    public Tablero (int tamano){
+        this.tamano = tamano;
+        this.cartas = new Carta[tamano][tamano];
+        this.initTablero (tamano);
+    }
+
+    public void initTablero (int tamano){
+        for (int i = 0; i < tamano; i++){
+            for (int ii = 0; ii< tamano; ii++ ){
+                this.cartas[i][ii] = new Carta(null, false, false);
+                Baraja.add(this.cartas[i][ii]);
+            }
+        }
+    }
+
+
+
 }
